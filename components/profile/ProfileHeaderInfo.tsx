1e8c6299ad9ee72b023203fbd6ea8ad4e808badb
yui949 <yuixicun97@gmail.com>
2025-08-24 04:30:34 +0900

    プロフィール情報のコンポーネントを作成

---
+++ components/profile/ProfileHeaderInfo.tsx
@@ -0,0 +1,23 @@
+export default function ProfileHeaderInfo() {
+  return (
+<div>
+  <div className="text--[#757575]  text-xl font-zen">watnow.0123</div>
+    <div className="flex items-center pt-2">
+        <img src="default_user icon.png" />
+
+        <div>
+            <div className="text-[#757575]  text-xl font-zen" >11</div>
+            <div className="text-[#757575] text-xl font-zen">投稿</div>
+        </div> 
+        <div className="px-4">
+            <div className="text-[#757575] text-xl font-zen" >11</div>
+            <div className="text-[#757575] text-xl font-zen">フォロワー</div>
+        </div>
+         <div>
+            <div className="text-[#757575] text-xl font-zen" >11</div>
+            <div className="text-[#757575] text-xl font-zen">フォロー中</div>
+        </div>
+    </div>
+    </div>
+  );
+}
