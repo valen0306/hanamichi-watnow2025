193a6b67b6b0d5ec247017ac1e2c9eb30a3ff2ea
yuuuki16 <cp0268ki@ed.ritsumei.ac.jp>
2025-08-24 02:55:50 +0900

    　”header”

---
+++ components/features/camera/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostHeader = () => {
+  return (
+    <header className="post-header">
+      <h1 className="post-title">新規投稿</h1>
+    </header>
+  );
+};
+
+export default PostHeader;
